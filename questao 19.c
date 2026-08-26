f07f6b91b8fea1c9d2603329de64011a879f436c
jlwvl-droid <jlwvl@cesar.school>
2026-08-26 19:19:18 -0300

    Create questao 19.c

---
+++ questao 19.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("um\n dois\n  tres");
+    
+    return 0;
+}
+//fiquei em duvida se era para usar o \t, mas apenas um espaço fica mais parecido com o resultado
